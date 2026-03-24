2d4fe9fd3585d030c3f7c71c1bec8d889a65394c
zikenn26 <gulnayak1206@gmail.com>
2026-03-25 02:50:09 +0530

    🔧 Configured auth components to force redirect to dashboard

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-background">
-      <SignUp />
+      <SignUp forceRedirectUrl="/dashboard" />
     </div>
   );
 }
